706e36850ad2f5ce5bf80ef2d4be397d8f830f1e
Zahscha <zahscha.gonzalez@gmail.com>
2020-12-23 16:48:46 -0500

    Remove irrelevant comments

--- run_analysis.R
+++ run_analysis.R
@@ -1,18 +1,3 @@
-##    The submitted data set is tidy.
-## The Github repo contains the required scripts.
-## GitHub contains a code book that modifies and updates the available codebooks with the data to indicate all the variables and summaries calculated, along with units, and any other relevant information.
-## The README that explains the analysis files is clear and understandable.
-## The work submitted for this project is the work of the student who submitted it.
-
-## Verification Criteria
-## ===================================================================================
-## [X]Merges the training and the test sets to create one data set.
-## [X]Extracts only the measurements on the mean and standard deviation for each measurement.
-## [X]Uses descriptive activity names to name the activities in the data set
-## [X]Appropriately labels the data set with descriptive variable names.
-## [X]From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
-## ====================================================================================
-
 # Load dplyr library
 library(dplyr)
 
